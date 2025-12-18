1abaa5ed912ae40da7511f68d57791d5d1f620f9
nowakczarek <czarekkawon@onet.pl>
2025-12-18 18:54:02 +0100

    small cosmetic changes

--- Frontend/crm-frontend/src/app/layout/layout.ts
+++ Frontend/crm-frontend/src/app/layout/layout.ts
@@ -1,11 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { Auth } from '../auth/services/auth';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatButtonModule, MatIconModule],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
@@ -13,6 +16,19 @@ export class Layout {
   
   private auth = inject(Auth)
   private router = inject(Router)
+  private iconRegistry = inject(MatIconRegistry)
+  private sanitizer = inject(DomSanitizer)
+  
+  ngOnInit(){
+    this.iconRegistry.addSvgIcon(
+      'logout_icon.svg',
+      this.sanitizer.bypassSecurityTrustResourceUrl('logout_icon.svg')
+    )
+    this.iconRegistry.addSvgIcon(
+      'search_icon.svg',
+      this.sanitizer.bypassSecurityTrustResourceUrl('search_icon.svg')
+    )
+  }
 
   logout(){
     this.auth.logout()
