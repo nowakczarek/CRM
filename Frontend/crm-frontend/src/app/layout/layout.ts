2ab34863fbec40c6558497735e579f95fb42af42
nowakczarek <czarekkawon@onet.pl>
2025-12-27 20:00:24 +0100

    fix topbar buttons

--- Frontend/crm-frontend/src/app/layout/layout.ts
+++ Frontend/crm-frontend/src/app/layout/layout.ts
@@ -2,8 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { Auth } from '../auth/services/auth';
 import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-layout',
@@ -16,20 +15,7 @@ export class Layout {
   
   private auth = inject(Auth)
   private router = inject(Router)
-  private iconRegistry = inject(MatIconRegistry)
-  private sanitizer = inject(DomSanitizer)
   
-  ngOnInit(){
-    this.iconRegistry.addSvgIcon(
-      'logout_icon.svg',
-      this.sanitizer.bypassSecurityTrustResourceUrl('logout_icon.svg')
-    )
-    this.iconRegistry.addSvgIcon(
-      'search_icon.svg',
-      this.sanitizer.bypassSecurityTrustResourceUrl('search_icon.svg')
-    )
-  }
-
   logout(){
     this.auth.logout()
     this.router.navigate(['/login'])
